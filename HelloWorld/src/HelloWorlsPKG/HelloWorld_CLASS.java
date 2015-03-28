f7cfec02f066e6a8be4ef2bca458d56b57bb09ee
Xuser <Xuser@BFG>
2015-03-29 00:19:21 +0200

    ok

---
+++ HelloWorld/src/HelloWorlsPKG/HelloWorld_CLASS.java
@@ -0,0 +1,13 @@
+package HelloWorlsPKG;
+
+public class HelloWorld_CLASS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Worl add some code! - MASTER");
+		System.out.println("add some code ...");
+
+	}
+
+}
